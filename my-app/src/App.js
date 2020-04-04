c0ff1c6ea366fbd98ae3e008408626ab64ce4524
KatAlilo <alilovickata@ufl.edu>
2020-04-04 22:53:44 +0000

    app

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      "Best way to spend your time during self-isolation by Kata Alilovic"
+        <img src="https://i2.wp.com/metro.co.uk/wp-content/uploads/2020/02/Screenshot_2-37f5.jpg?quality=90&strip=all&zoom=1&resize=644%2C363&ssl=1" className="App-logo" alt="logo" />
+        <p>
+          Animal Crossing-New Horizons!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.animal-crossing.com/new-horizons/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Buy here!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
